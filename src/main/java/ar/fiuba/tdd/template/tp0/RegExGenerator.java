36c7c4ed613ad373b8207b5f6ec80c8fc1960332
Nicolás Keklikian <nkeklikian@gmail.com>
2016-03-21 03:06:08 -0300

    Resolución del TP 0

--- src/main/java/ar/fiuba/tdd/template/tp0/RegExGenerator.java
+++ src/main/java/ar/fiuba/tdd/template/tp0/RegExGenerator.java
@@ -2,22 +2,107 @@ package ar.fiuba.tdd.template.tp0;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class RegExGenerator {
-    // TODO: Uncomment this field
-    //private int maxLength;
+    private int maxLength;
+    private Random random = new Random();
+    private StringBuffer resultBuffer;
+    private static int INVALID_CHAR = 133;
+    private StringBuffer charsInToken;
 
-    //public RegExGenerator(int maxLength) {
-    //    this.maxLength = maxLength;
-    //}
+    public RegExGenerator(int maxLength) {
+        this.maxLength = maxLength;
+    }
 
-    // TODO: Uncomment parameters
-    public List<String> generate(/*String regEx, int numberOfResults*/) {
+    private void processRepeatingQuantifiers(char charAtIndex) {
+        switch (charAtIndex) {
+            case '*':
+                int totalRepetitions = random.nextInt(maxLength);
+                if (totalRepetitions == 0) {
+                    resultBuffer.deleteCharAt(resultBuffer.length() - 1);
+                } else {
+                    for (int repetition = 0; repetition < totalRepetitions; repetition++) {
+                        resultBuffer.append(charsInToken.charAt(random.nextInt(charsInToken.length())));
+                    }
+                }
+                return ;
+            case '+':
+                totalRepetitions = random.nextInt(maxLength);
+                for (int repetition = 0; repetition < totalRepetitions; repetition++) {
+                    resultBuffer.append(charsInToken.charAt(random.nextInt(charsInToken.length())));
+                }
+                return ;
+            default:
+        }
+    }
+
+    //Takes care of ?, delegates other quantifiers
+    private void processQuantifiers(char charAtIndex) {
+        switch (charAtIndex) {
+            case '?':
+                boolean removeChar = random.nextInt(2) == 1;
+                if (removeChar) {
+                    resultBuffer.deleteCharAt(resultBuffer.length() - 1);
+                }
+                return;
+            default:
+                processRepeatingQuantifiers(charAtIndex);
+        }
+    }
+
+    //Takes care of . and literals
+    private void processSingleChar(char charAtIndex) {
+        if (charAtIndex == '+' || charAtIndex == '?' || charAtIndex == '*') {
+            processQuantifiers(charAtIndex);
+        } else {
+            switch (charAtIndex) {
+                case '.':
+                    int charToPut = INVALID_CHAR;
+                    while (charToPut == INVALID_CHAR) {
+                        //The first 32 ASCII chars are invalid
+                        charToPut = (random.nextInt(256 - 32) + 32);
+                    }
+                    charsInToken = new StringBuffer("" + (char) charToPut);
+                    resultBuffer.append((char) charToPut);
+                    return;
+                default:
+                    charsInToken = new StringBuffer("" + charAtIndex);
+                    resultBuffer.append(charAtIndex);
+            }
+        }
+    }
+
+    private String generateOneResult(String regEx) {
+        resultBuffer = new StringBuffer();
+        int index = 0;
+        for (;index < regEx.length(); index++) {
+            switch (regEx.charAt(index)) {
+                case '\\':
+                    index++;
+                    resultBuffer.append(regEx.charAt(index));
+                    charsInToken = new StringBuffer("" + regEx.charAt(index));
+                    break;
+                case '[':
+                    for (charsInToken = new StringBuffer(); regEx.charAt(index + 1) != ']'; index++) {
+                        charsInToken.append(regEx.charAt(index + 1));
+                    }
+                    resultBuffer.append(charsInToken.charAt(random.nextInt(charsInToken.length())));
+                    index++;
+                    break;
+                default :
+                    processSingleChar(regEx.charAt(index));
+            }
+        }
+        return resultBuffer.toString();
+    }
+
+    public List<String> generate(String regEx, int numberOfResults) {
         return new ArrayList<String>() {
             {
-                add("a");
-                add("b");
-                add("c");
+                for (int resultsAdded = 0; resultsAdded < numberOfResults; resultsAdded++) {
+                    add(generateOneResult(regEx));
+                }
             }
         };
     }
--- src/test/java/ar/fiuba/tdd/template/tp0/RegExGeneratorTest.java
+++ src/test/java/ar/fiuba/tdd/template/tp0/RegExGeneratorTest.java
@@ -11,9 +11,9 @@ import static org.junit.Assert.assertTrue;
 public class RegExGeneratorTest {
 
     private boolean validate(String regEx, int numberOfResults) {
-        RegExGenerator generator = new RegExGenerator();
-        // TODO: Uncomment parameterss
-        List<String> results = generator.generate(/*regEx, numberOfResults*/);
+        RegExGenerator generator = new RegExGenerator(5);
+        List<String> results = generator.generate(regEx, numberOfResults);
+        System.out.println(results.get(0));
         // force matching the beginning and the end of the strings
         Pattern pattern = Pattern.compile("^" + regEx + "$");
         return results
@@ -26,8 +26,6 @@ public class RegExGeneratorTest {
                     (item1, item2) -> item1 && item2);
     }
 
-    //TODO: Uncomment these tests
-    /*
     @Test
     public void testAnyCharacter() {
         assertTrue(validate(".", 1));
@@ -62,6 +60,30 @@ public class RegExGeneratorTest {
     public void testCharacterSetWithQuantifiers() {
         assertTrue(validate("[abc]+", 1));
     }
-    */
-    // TODO: Add more tests!!!
+
+    @Test
+    public void testLiteralBracket(){
+        assertTrue(validate("\\]",1));
+    }
+
+    @Test
+    public void testLiteralBackslash(){
+        assertTrue(validate("\\\\", 1));
+    }
+
+    //Test if any invalid chars are returned
+    @Test
+    public void testTwentyDots(){
+        assertTrue(validate("....................", 1));
+    }
+
+    @Test
+    public void testDigits(){
+        assertTrue(validate("1356789420",1));
+    }
+
+    @Test
+    public void testMultipleQuantifiers(){
+        assertTrue(validate(".*a+[guante]?[xkcd]+1024?n*s*", 1));
+    }
 }
